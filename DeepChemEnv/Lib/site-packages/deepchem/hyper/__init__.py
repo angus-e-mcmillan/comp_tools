258b5ad923334cc59de3a85c88efe33e10afd09d
angus-e-mcmillan <angus.emcmillan@gmail.com>
2024-06-19 12:51:10 +0200

    Enviroment update

--- DeepChemEnv/Lib/site-packages/deepchem/hyper/__init__.py
+++
@@ -1,4 +0,0 @@
-# flake8: noqa
-from deepchem.hyper.base_classes import HyperparamOpt
-from deepchem.hyper.grid_search import GridHyperparamOpt
-from deepchem.hyper.gaussian_process import GaussianProcessHyperparamOpt
--- DeepChemEnv/Lib/site-packages/deepchem/metalearning/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-try:
-  from deepchem.metalearning.maml import MAML, MetaLearner
-except ModuleNotFoundError:
-  pass
