2b230447aa23f464fe024823cda817069d74305d
angus-e-mcmillan <angus.emcmillan@gmail.com>
2024-07-15 14:42:19 +0200

    Loops complete

--- 06-Kaggle/plates/plates.py
+++ 06-Kaggle/plates/plates.py
@@ -9,11 +9,33 @@ def main():
 def is_valid(s):
     length = len(s)
     if length < 3:
-        print("Invalid")
+        print("Invalid_short")
+        return False
     elif length > 6:
-        print("Invalid")
+        print("Invalid_long")
+        return False
+    elif s.isalnum() != True:
+        print("Invalid_punct")
+        return False
+    elif (s[0].isdigit()) and ((s[1:]).isalpha() is False):
+        print("Invalid_slice1")
+        return False
+    elif (s[1].isdigit()) and ((s[2:]).isalpha() is False):
+        print("Invalid_slice2")
+        return False
+    elif (s[2].isdigit()) and ((s[3:]).isalpha() is False):
+        print("Invalid_slice3")
+        return False
+    elif (s[3].isdigit()) and ((s[4:]).isalpha() is False):
+        print("Invalid_slice2")
+        return False
+    elif (s[4].isdigit()) and ((s[5:]).isalpha() is False):
+        print("Invalid_slice1")
+        return False
+    elif (s[5].isdigit()) and ((s[6:]).isalpha() is False):
+        print("Invalid_slice2")
+        return False
     else:
-        print("Valid")
-
-
+        return True
 main()
+
